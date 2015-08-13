7c85d95ba59476fb232e8d63855678979a281ffb
Arnas Juskevicius <JuskeviciusArn@gmail.com>
2015-08-13 17:17:56 +0300

    updated validation

--- js/validation.js
+++ js/validation.js
@@ -1,21 +1,36 @@
 document.getElementById("submit").addEventListener("click", checkIsValid);
 
 function checkIsValid() {
+		getResult = new Array(12);
 	var name = document.getElementById("name"),
 		lastN = document.getElementById("lastN"),
 		msg = document.getElementById("msg"),
-		failMessage = alert("Please fill all fields"),
 		testData =[name, lastN, msg],
-		badValues = ["enter your name","enter your last name", ""];
+		badValues = ["enter your name","enter your last name", ""],
+		check = 0;
+
 
 		for(var i = 0; i < testData.length; i++){
 			if(testData[i].value.toLowerCase() === badValues[i].toLowerCase()){
 				testFailed(testData[i]);
-				failMessage;
 			}
 			else {
 				testSuccess(testData[i]);
 			}
+		}
+
+		for(var i = 0; i < getResult.length; i++){
+			if(getResult[i] === "successful"){
+				check++;
+			}
+		}
+
+		if(check === 3) {
+			alert("Message successfly sent");
+			getResult = [];
+		}
+		else {
+			getResult = [];
 		}
 }
 
@@ -23,9 +38,11 @@ function checkIsValid() {
 function testFailed(fieldName) {
 	fieldName.style.borderColor = "#cc0066"
 	fieldName.style.backgroundColor = "#FEEDF5";
+	getResult.push("failed")
 }
 
 function testSuccess(fieldName) {
 	fieldName.style.borderColor = "#999"
 	fieldName.style.backgroundColor = "#fff";
+	getResult.push("successful")
 }
